fa792c653532b843175afec503828110e55468c0
yupeiliang <yupeiliang@fotoable.com>
2020-10-26 11:46:50 +0800

    Initial Commit

---
+++ DeviceCapture/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  DeviceCapture
+//
+//  Created by app-01 on 2020/10/26.
+//  Copyright © 2020 app-01 org. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ DeviceCapture/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DeviceCapture
+//
+//  Created by app-01 on 2020/10/26.
+//  Copyright © 2020 app-01 org. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
